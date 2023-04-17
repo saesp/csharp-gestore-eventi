d2e5f92056eeadb2ecc936f71697529fe0ff4b3f
Salvatore Esposito <esposito.00@outlook.it>
2023-04-17 12:17:20 +0200

    added methods of Event class

--- GestoreEventi/Event.cs
+++ GestoreEventi/Event.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-
 namespace GestoreEventi
 {
     public class Event
@@ -29,7 +28,7 @@ namespace GestoreEventi
             get { return _date; }
             set
             {
-                if (value < DateTime.Now)
+                if (value > DateTime.Now)
                     _date = value;
                 else
                     Console.WriteLine("La data inserita non è valida");
@@ -47,14 +46,39 @@ namespace GestoreEventi
             }
         }
 
-        public int ReservedSeatsNumber { get; }
-
+        public int ReservedSeatsNumber { get; set; }
 
         public Event (string title, DateTime date, int seatsNumber)
         {
             Title = title;
             Date = date;
             SeatsNumber = seatsNumber;
+            ReservedSeatsNumber = 0; 
+        }
+        
+        public void AddReservedSeats(int value)
+        {
+            int availableSeats = SeatsNumber - ReservedSeatsNumber; 
+            if (Date > DateTime.Now && value <= availableSeats)
+                ReservedSeatsNumber = value + ReservedSeatsNumber;
+            else
+                Console.WriteLine("Il numero non è valido");
+        }
+
+        public void RemoveReservedSeats(int value)
+        {
+            if (Date > DateTime.Now && value <= ReservedSeatsNumber)
+                //ReservedSeatsNumber = value - ReservedSeatsNumber;
+                ReservedSeatsNumber -= value;
+            else
+                Console.WriteLine("Il numero non è valido");
+        }
+
+        public override string ToString()
+        {
+            var text = $"{Date.ToString("dd/MM/yyyy")} - {Title}";
+
+            return text; 
         }
     }
 }
