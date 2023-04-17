445facdd03db6b1b7301f928cde8a34c68d293de
Salvatore Esposito <esposito.00@outlook.it>
2023-04-17 23:13:02 +0200

    added comments

--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 
 
+//dati nuovo evento
 Console.WriteLine("Inserisci un nuovo evento");
 
 Console.Write("Titolo evento: ");
@@ -19,19 +20,21 @@ Console.Write($"Quanti posti desideri prenotare? ");
 int reservedSeatsNumberUser = Convert.ToInt32(Console.ReadLine());
 Console.Write("\n");
 
-
+//calcolo posti disponibili
 int availableSeatsNumber = seatsNumberUser - reservedSeatsNumberUser;
 
+//stampare posti prenotati e disp
 Console.WriteLine($"Numero posti prenotati: {reservedSeatsNumberUser}");
 Console.WriteLine($"Numero posti ancora disponibili: {availableSeatsNumber} \n");
 
 
+//ciclo per disdire posti prenotati
 int removedSeatsNumberUser = 0;
 
 Console.Write($"Vuoi disdire dei posti per l'evento {titleUser}? (Yes/No) ");
 string answerUser = Console.ReadLine();
 
-while (answerUser == "Yes") {
+while (answerUser == "Yes" || answerUser == "yes") {
     Console.Write($"Quanti posti vuoi disdire? ");
     int _removedSeatsNumberUser = Convert.ToInt32(Console.ReadLine());
     Console.Write("\n");
@@ -42,6 +45,7 @@ while (answerUser == "Yes") {
     removedSeatsNumberUser += _removedSeatsNumberUser; 
 }
 
+//se answerUser è no
 Console.WriteLine("Va bene!");
 Console.Write("\n");
 
@@ -49,17 +53,21 @@ Console.WriteLine($"Numero posti disponibili: {availableSeatsNumber + removedSea
 Console.WriteLine($"Numero posti prenotati: {reservedSeatsNumberUser - removedSeatsNumberUser} \n");
 
 
+
+//Programma eventi
+
+//lista eventi
 List<Event> eventsList = new List<Event>();
 
+//dati programma
 Console.Write("Titolo del tuo programma eventi: ");
 string titleEventPlanUser = Console.ReadLine();
-Console.WriteLine("\n");
 
 Console.Write("Quanti eventi vuoi aggiungere al tuo programma? ");
 int eventsNumbereUser = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("\n");
 
-
+//dati eventi
 for (int i = 0; i < eventsNumbereUser; i++)
 {
     Console.WriteLine($"Inserisci l'evento {i}");
@@ -78,13 +86,14 @@ for (int i = 0; i < eventsNumbereUser; i++)
     Console.Write("\n");
 }
 
+//numero eventi
 EventsPlan.countEventsPlan(eventsList);
 Console.Write("\n");
 
-
+//stampa programma eventi
 EventsPlan.writeEventsPlan(titleEventPlanUser, eventsList);
 Console.Write("\n");
 
-
+//elimina eventi
 EventsPlan.clearEventsList(eventsList);
 Console.Write("\n");
