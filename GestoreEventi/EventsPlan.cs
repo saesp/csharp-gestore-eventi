12f23c7e21683a2b0b8a3861fa7a91ef236a95f3
Salvatore Esposito <esposito.00@outlook.it>
2023-04-17 22:53:38 +0200

    completed

---
+++ GestoreEventi/EventsPlan.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GestoreEventi
+{
+    public class EventsPlan
+    {
+        public string Title {get; set;}
+        public List<Event> Events { get; set;}
+
+        public EventsPlan (string title, List<Event> events)
+        {
+            Title = title;
+            Events = new List<Event>();
+        }
+        
+        public void addEvent(Event e)
+        {
+            Events.Add(e);
+        }
+
+        public static void writeEventsList(List<Event> list)
+        {
+            foreach (var i in list) {
+                Console.WriteLine($"{i}");
+            }
+        }
+
+        public static void countEventsPlan(List<Event> list)
+        {
+            Console.WriteLine($"Il numero degli eventi dal tuo programma è {list.Count}");
+        }
+
+        public static void writeEventsPlan(string title, List<Event> list)
+        {
+            Console.WriteLine($"Ecco il tuo PROGRAMMA EVENTI: ");
+
+            Console.WriteLine($"{title}");
+            foreach (var i in list)
+            {
+                Console.WriteLine($"{i.Date} - {i.Title}");
+            }
+        }
+
+        //Clear
+        public static void clearEventsList(List<Event> list)
+        {
+            Console.WriteLine("Elimino eventi dal tuo programma");
+            list.Clear();
+        }
+    }
+}
--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,4 +1,5 @@
 ﻿using GestoreEventi;
+using System.Collections.Generic;
 using System.ComponentModel;
 
 
@@ -12,31 +13,78 @@ DateTime dateUser = Convert.ToDateTime(Console.ReadLine());
 
 Console.Write("Numero massimo di posti: ");
 int seatsNumberUser = Convert.ToInt32(Console.ReadLine());
-
 Console.Write("\n");
 
-
-Console.Write($"Per l'evento {titleUser} quanti posti vuoi prenotare? ");
+Console.Write($"Quanti posti desideri prenotare? ");
 int reservedSeatsNumberUser = Convert.ToInt32(Console.ReadLine());
 Console.Write("\n");
 
 
-List<Event> eventList = new List<Event>();
-Event event1 = new Event(titleUser, dateUser, seatsNumberUser, reservedSeatsNumberUser);
-eventList.Add(event1);
-
-
-int availableSeatsNumber = seatsNumberUser - reservedSeatsNumberUser; 
+int availableSeatsNumber = seatsNumberUser - reservedSeatsNumberUser;
 
 Console.WriteLine($"Numero posti prenotati: {reservedSeatsNumberUser}");
 Console.WriteLine($"Numero posti ancora disponibili: {availableSeatsNumber} \n");
 
 
-Console.Write($"Per l'evento {titleUser} quanti posti vuoi disdire? ");
-int removedSeatsNumberUser = Convert.ToInt32(Console.ReadLine());
-Console.Write("\n");
+int removedSeatsNumberUser = 0;
 
+Console.Write($"Vuoi disdire dei posti per l'evento {titleUser}? (Yes/No) ");
+string answerUser = Console.ReadLine();
+
+while (answerUser == "Yes") {
+    Console.Write($"Quanti posti vuoi disdire? ");
+    int _removedSeatsNumberUser = Convert.ToInt32(Console.ReadLine());
+    Console.Write("\n");
+
+    Console.Write($"Vuoi disdire dei posti per l'evento {titleUser}? (Yes/No) ");
+    answerUser = Console.ReadLine();
+
+    removedSeatsNumberUser += _removedSeatsNumberUser; 
+}
+
+Console.WriteLine("Va bene!");
+Console.Write("\n");
 
 Console.WriteLine($"Numero posti disponibili: {availableSeatsNumber + removedSeatsNumberUser}");
 Console.WriteLine($"Numero posti prenotati: {reservedSeatsNumberUser - removedSeatsNumberUser} \n");
 
+
+List<Event> eventsList = new List<Event>();
+
+Console.Write("Titolo del tuo programma eventi: ");
+string titleEventPlanUser = Console.ReadLine();
+Console.WriteLine("\n");
+
+Console.Write("Quanti eventi vuoi aggiungere al tuo programma? ");
+int eventsNumbereUser = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("\n");
+
+
+for (int i = 0; i < eventsNumbereUser; i++)
+{
+    Console.WriteLine($"Inserisci l'evento {i}");
+
+    Console.Write("Titolo evento: ");
+    string titleUser_ = Console.ReadLine();
+
+    Console.Write("Data evento (gg/mm/yyyy): ");
+    DateTime dateUser_ = Convert.ToDateTime(Console.ReadLine());
+
+    Console.Write("Numero massimo di posti: ");
+    int seatsNumberUser_ = Convert.ToInt32(Console.ReadLine());
+
+    Event event_ = new Event(titleUser_, dateUser_, seatsNumberUser_, 0);
+    eventsList.Add(event_);
+    Console.Write("\n");
+}
+
+EventsPlan.countEventsPlan(eventsList);
+Console.Write("\n");
+
+
+EventsPlan.writeEventsPlan(titleEventPlanUser, eventsList);
+Console.Write("\n");
+
+
+EventsPlan.clearEventsList(eventsList);
+Console.Write("\n");
